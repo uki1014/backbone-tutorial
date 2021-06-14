d21d44de29f7737281f05c4270964ec6ed437489
uki <yucchini.yuki1014@gmail.com>
2021-06-14 23:21:08 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## 令和の Backbone.js Tutorial
+
+#### Getting Start
+
+```shell
+$ git clone https://github.com/uki1014/backbone-tutorial
+$ cd backbone-tutorial
+$ npm install
+$ npm run watch
+```
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: 'development',
+  // エントリーポイントの.jsファイル
+  entry: `./src/index.js`,
+  // ファイルの出力設定
+  output: {
+    // 出力ファイルのディレクトリ名
+    path: `${__dirname}/dist`,
+    // 出力ファイル名
+    filename: 'app.js',
+  },
+  //webpack-dev-server設定
+  devServer: {
+    contentBase: 'dist',
+    open: true,
+    port: 3000,
+  },
+};
